d913c53438b531f1cb9e1fdd5292b9f9060cc080
Veronica Aranibar Flores <varaniba@f1r1s11.codam.nl>
2026-06-02 18:21:45 +0200

    changed when input is unknown unit type, but not sure if output is as expecter (needs to print seed type?)

--- Python_00/ex7/ft_seed_inventory.py
+++ Python_00/ex7/ft_seed_inventory.py
@@ -6,6 +6,6 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
             return f"{quantity} grams total"
         if unit == "area":
             return f"covers {quantity} square meters"
-        return f"{quantity} {unit}"
+        return "Unknown unit type"
     unit_converted = unit_converter(quantity, unit)
     print(f"{seed_type.capitalize()} seeds: {unit_converted}")
