c4eb1a511583dc1fbacb12ec0f629045f26a6d0e
Veronica Aranibar Flores <varaniba@f1r2s19.codam.nl>
2026-06-03 18:00:14 +0200

    added variables

---
+++ Python_01/ex0/ft_garden_intro.py
@@ -0,0 +1,7 @@
+def ft_garden_intro(name: str, height: int, age: int):
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print("=== End of Program ===")
+ft_garden_intro("Rose", 25, 30)
