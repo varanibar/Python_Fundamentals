b7f03fcb71594a64a99a5036e8ed19d44a5026bc
Veronica Aranibar Flores <varaniba@f1r2s19.codam.nl>
2026-06-03 10:26:59 +0200

    fixed ex7

--- Python_00/ex7/ft_seed_inventory.py
+++ Python_00/ex7/ft_seed_inventory.py
@@ -1,11 +1,10 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
-    def unit_converter(quantity, unit):
-        if unit == "packets":
-            return f"{quantity} packets available"
-        if unit == "grams":
-            return f"{quantity} grams total"
-        if unit == "area":
-            return f"covers {quantity} square meters"
-        return "Unknown unit type"
-    unit_converted = unit_converter(quantity, unit)
-    print(f"{seed_type.capitalize()} seeds: {unit_converted}")
+    seed_type = seed_type.capitalize()
+    if unit == "packets":
+        print(f"{seed_type} seeds: {quantity} packets available")
+    elif unit == "grams":
+        print(f"{seed_type} seeds: {quantity} grams total")
+    elif unit == "area":
+        print(f"{seed_type} seeds: covers {quantity} square meters")
+    else:
+        print("Unknown unit type")
