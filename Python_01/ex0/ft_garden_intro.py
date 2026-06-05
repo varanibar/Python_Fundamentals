728247538afd7fe0be4284269930fa5c48cbf148
Veronica Aranibar Flores <varaniba@f1r4s5.codam.nl>
2026-06-05 14:22:18 +0200

    working version

--- Python_01/ex0/ft_garden_intro.py
+++ Python_01/ex0/ft_garden_intro.py
@@ -1,7 +1,14 @@
-def ft_garden_intro(name: str, height: int, age: int):
+def ft_garden_intro():
     print("=== Welcome to My Garden ===")
+    name = "Rose"
+    height = 25
+    age = 30
     print(f"Plant: {name}")
     print(f"Height: {height}cm")
     print(f"Age: {age} days")
+    print("")
     print("=== End of Program ===")
-ft_garden_intro("Rose", 25, 30)
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
