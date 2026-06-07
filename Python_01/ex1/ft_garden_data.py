a0fb90c316bbeec672742eb54254ddb321dee00b
Veronica Aranibar Flores <varaniba@f1r6s5.codam.nl>
2026-06-07 16:33:33 +0200

    added type annotations to variables

--- Python_01/ex1/ft_garden_data.py
+++ Python_01/ex1/ft_garden_data.py
@@ -1,14 +1,14 @@
 class Plant:
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         self.height = height
         self.age = age
 
-    def show(self):
+    def show(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
 
 
-def ft_garden_data():
+def ft_garden_data() -> None:
     rose = Plant("Rose", 25, 30)
     sunflower = Plant("Sunflower", 80, 45)
     cactus = Plant("Cactus", 15, 20)
