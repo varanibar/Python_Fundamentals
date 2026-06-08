235cbae6dbce12ae6f3066782761ca9c474f99b2
Veronica Aranibar Flores <varaniba@f1r1s17.codam.nl>
2026-06-08 14:20:31 +0200

    changed code to use set functions, now works as expected, passes flake8 and mypy strict

--- Python_01/ex4/ft_garden_security.py
+++ Python_01/ex4/ft_garden_security.py
@@ -1,23 +1,15 @@
 class Plant:
-    def __init__(self, name: str, height: float, age: int, growth: float = 0) -> None:
+    def __init__(
+        self,
+        name: str,
+        height: float,
+        age: int
+                ) -> None:
         self._name = name.capitalize()
-        if height > 0:
-            self._height = round(height, 1)
-        else:
-            self._height = 0
-            print(f"{self._name}: Error, height can't be negative")
-        if age > 0:
-            self._age = age
-        else:
-            self._age = 0
-            print(f"{self._name}: Error, age can't be negative")
-        if growth == 0:
-            self._growth = round(height / age, 1)
-        else:
-            self._growth = growth
-
-    def grow(self) -> None:
-        self._height = round(self._height + self._growth, 1)
+        self._height = 0.0
+        self.set_height(height)
+        self._age = 0
+        self.set_age(age)
 
     def get_height(self) -> float:
         return self._height
@@ -26,36 +18,41 @@ class Plant:
         return self._age
 
     def set_height(self, new_height: float) -> None:
-        if new_height > 0:
+        if new_height > 0.0 and self._height == 0.0:
+            self._height = round(new_height, 1)
+        elif new_height > 0.0:
             self._height = round(new_height, 1)
             print(f"Height updated: {self._height}cm")
         else:
             print(f"{self._name}: Error, height can't be negative")
-            print("Height update rejected")
+            if self._height != 0:
+                print("Height update rejected")
 
     def set_age(self, new_age: int) -> None:
-        if new_age > 0:
+        if new_age > 0 and self._age == 0:
+            self._age = new_age
+        elif new_age > 0:
             self._age = new_age
             print(f"Age updated: {self._age} days")
         else:
             print(f"{self._name}: Error, age can't be negative")
-            print("Age update rejected")
+            if self._age != 0:
+                print("Age update rejected")
 
     def show(self) -> None:
-        if self._height > 0 and self._age > 0:
-            print(f"{self._name}: {self._height}cm, {self._age} days old")
+        print(f"{self._name}: {self._height}cm, {self._age} days old")
 
 
-def ft_garden_security():
+def ft_garden_security() -> None:
     print("=== Garden Security System ===")
     plant = Plant("Rose", 15.0, 10)
     print("Plant created: ", end="")
     plant.show()
     print("")
-    plant.set_height(25)
+    plant.set_height(25.0)
     plant.set_age(30)
     print("")
-    plant.set_height(-10)
+    plant.set_height(-10.0)
     plant.set_age(-50)
     print("")
     print("Current state: ", end="")
