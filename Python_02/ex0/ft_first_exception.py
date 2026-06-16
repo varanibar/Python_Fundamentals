779bc8f649822733721e4975ae2fabbfb08053a1
Veronica Aranibar Flores <varaniba@f1r4s2.codam.nl>
2026-06-16 14:30:44 +0200

    working version

---
+++ Python_02/ex0/ft_first_exception.py
@@ -0,0 +1,23 @@
+def input_temperature(temp_str: str) -> int:
+    return int(temp_str)
+
+
+def test_temperature(temp_str: str) -> None:
+    print(f"\nInput data is '{temp_str}'")
+    try:
+        temp = input_temperature(temp_str)
+        print(f"Temperature is now {temp}°C")
+    except ValueError as err:
+        print("Caught input_temperature error: ", end="")
+        print(f"{err}")
+
+
+def ft_first_exception() -> None:
+    print("=== Garden Temperature ===")
+    test_temperature("25")
+    test_temperature("abc")
+    print("\nAll tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+    ft_first_exception()
