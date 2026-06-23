b70ebe6dd637f2b2e0dd1df10f5c29bdb26a5e9b
Veronica Aranibar Flores <varaniba@f1r1s18.codam.nl>
2026-06-23 20:01:59 +0200

    working version, but new item update might need validation too

--- Python_03/ex4/ft_inventory_system.py
+++ Python_03/ex4/ft_inventory_system.py
@@ -1,64 +1,73 @@
 import sys
 
-def ft_inventory_system() -> None:
-    argc = len(sys.argv)
-    if argc == 1:
-        print("No arguments provided")
-    else:
-        dict_items = dict()
+
+class Inventory:
+    def __init__(self) -> None:
+        self.dict_items = self.ft_inventory_system()
+
+    def ft_inventory_system(self) -> dict[str, int]:
+        dict_items: dict[str, int] = dict()
         for arg in sys.argv[1:]:
-            # print("\n")
-            # print (arg)
-            lst_object = arg.split(":")
-            if len(lst_object) != 2:
+            argv = arg.split(":")
+            if len(argv) != 2:
                 print(f"Error - invalid parameter '{arg}'")
             else:
                 for key in dict_items.keys():
-                    # print(f"{lst_object[0]} vs {key}")
-                    if lst_object[0] == key:
-                        print(f"Redundant item '{lst_object[0]}' - discarding")
+                    if argv[0] == key:
+                        print(f"Redundant item '{argv[0]}' - discarding")
                         break
                 else:
-                    # print("ELSE")
                     try:
-                        key = lst_object[0].lower()
-                        value = int(lst_object[1])
-                        if value <= 0:
-                            raise ValueError(f"quantity must be a positive number '{value}'")
-                        dict_items[key] = value
+                        key = argv[0].lower()
+                        value = int(argv[1])
+                        if value < 0:
+                            raise ValueError(
+                                f"quantity must be a (+) number: '{value}'"
+                                )
                     except ValueError as err:
-                        print(f"Quantity error for '{lst_object[0]}': {err}")
+                        print(f"Quantity error for '{argv[0]}': {err}")
+                    else:
+                        dict_items[key] = value
+        return dict_items
 
-        # dict_items[object[0]] = {object[1]}
-        print(f"Got inventory: {dict_items}")
-        lst_items = list((dict_items.keys()))
+    def ft_inventory_stats(self) -> None:
+        print(f"Got inventory: {self.dict_items}")
+        lst_items = list((self.dict_items.keys()))
+        total_items = len(self.dict_items)
+        total_items_qty = sum(self.dict_items.values())
         print(f"Item list: {lst_items}")
-        total_items = len(dict_items)
-        total_items_qty = sum(dict_items.values())
         print(f"Total quantity of the {total_items} items: {total_items_qty}")
-        most_abundant_qty = 0
-        least_abundant_item = lst_items[0]
-        least_abundant_qty = dict_items[least_abundant_item]
-        for item in dict_items.keys():
-            item_qty = dict_items[item]
+        most_qty = 0
+        least_item = lst_items[0]
+        least_qty = self.dict_items[least_item]
+        for item in self.dict_items.keys():
+            item_qty = self.dict_items[item]
             n = round(item_qty * 100 / total_items_qty, 1)
             print(f"Item {item} represents {n}%")
-            if most_abundant_qty < item_qty:
-                most_abundant_item = item
-                most_abundant_qty = dict_items[item]
-            if least_abundant_qty > item_qty:
-                least_abundant_item = item
-                least_abundant_qty = dict_items[item]
-        print(f"Item most abundant: {most_abundant_item} with quantity {most_abundant_qty}")
-        print(f"Item least abundant: {least_abundant_item} with quantity {least_abundant_qty}")
+            if most_qty < item_qty:
+                most_item = item
+                most_qty = self.dict_items[item]
+            if least_qty > item_qty:
+                least_item = item
+                least_qty = self.dict_items[item]
+        print(f"Item most abundant: {most_item} with quantity {most_qty}")
+        print(f"Item least abundant: {least_item} with quantity {least_qty}")
 
-
-
+    def ft_inventory_update(self, new_item: dict[str, int]) -> None:
+        self.dict_items.update(new_item)
+        print(f"Updated inventory: {self.dict_items}")
 
 
 def main() -> None:
     print("=== Inventory System Analysis ===")
-    ft_inventory_system()
+    argc = len(sys.argv)
+    if argc == 1:
+        print("No arguments provided")
+    else:
+        inventory = Inventory()
+        inventory.ft_inventory_stats()
+        new_item = {"magic_item": 1}
+        inventory.ft_inventory_update(new_item)
 
 
 if __name__ == "__main__":
