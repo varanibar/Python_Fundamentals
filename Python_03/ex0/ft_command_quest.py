b232821ba946b7bbba9b92189df3c87a4d982128
Veronica Aranibar <mymindisfloating@gmail.com>
2026-06-26 12:20:11 +0200

    formatting

--- Python_03/ex0/ft_command_quest.py
+++ Python_03/ex0/ft_command_quest.py
@@ -5,12 +5,14 @@ def main() -> None:
     argc = len(sys.argv)
     print("=== Command Quest ===")
     print(f"Program name: {sys.argv[0]}")
+
     if argc == 1:
         print("No arguments provided!")
     else:
         print(f"Arguments received: {argc - 1}")
         for i in range(1, argc):
             print(f"Argument {i}: {sys.argv[i]}")
+
     print(f"Total arguments: {argc}\n")
 
 
