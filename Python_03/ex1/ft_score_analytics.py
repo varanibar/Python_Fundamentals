0b05823fc02d8f5aef40771ab90bb39910b13b50
Veronica Aranibar Flores <varaniba@f1r2s19.codam.nl>
2026-06-26 08:36:31 +0200

    formatting and changed if statement

--- Python_03/ex1/ft_score_analytics.py
+++ Python_03/ex1/ft_score_analytics.py
@@ -6,21 +6,29 @@ def main() -> None:
     print("=== Player Score Analytics ===")
     list_scores: list[int] = []
     list_invalid: list[str] = []
+
     for i in range(1, argc):
         try:
             nbr = int(sys.argv[i])
             list_scores.append(nbr)
         except Exception:
             list_invalid.append(sys.argv[i])
+
+    if len(list_invalid):
+        for invalid in list_invalid:
+            print(f"Invalid parameter: '{invalid}'")
+
     if len(list_scores):
         print("Scores processed: ", end="")
         print(list_scores)
         total_players = len(list_scores)
         print(f"Total players: {total_players}")
+
         total_score = 0
         for score in list_scores:
             total_score += score
         print(f"Total score: {total_score}")
+
         try:
             average_score = round(total_score / total_players, 1)
         except OverflowError as err:
@@ -31,9 +39,8 @@ def main() -> None:
         print(f"High score: {high_score}")
         print(f"Low score: {low_score}")
         print(f"Score range: {high_score - low_score}\n")
+
     else:
-        for invalid in list_invalid:
-            print(f"Invalid parameter: '{invalid}'")
         print("No scores provided. Usage: ", end="")
         print("python3 ft_score_analytics.py <score1> <score2> ...\n")
 
