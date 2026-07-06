dcecfeaf1b1e8dce3f5ccae35dac5457701182b4
Veronica Aranibar <mymindisfloating@gmail.com>
2026-07-06 13:28:25 +0200

    working version

---
+++ Python_04/ex0/ft_ancient_text.py
@@ -0,0 +1,25 @@
+import sys
+import typing
+
+
+def main() -> None:
+    if len(sys.argv) == 1:
+        print("Usage: ft_ancient_text.py <file>\n")
+    else:
+        print("=== Cyber Archives Recovery ===")
+        file_name = sys.argv[1]
+        print(f"Accessing file '{file_name}'")
+        try:
+            file: typing.IO[str] = open(file_name)
+        except Exception as err:
+            print(f"Error opening file '{file_name}': {err}\n")
+        else:
+            print("---\n")
+            print(file.read(), end="")
+            print("---")
+            file.close()
+            print(f"File '{file_name}' closed.")
+
+
+if __name__ == "__main__":
+    main()
